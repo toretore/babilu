f5358883926405a47248612362e88542a3d68631
Tore Darell <toredarell@gmail.com>
2008-12-05 15:51:20 +0100

    Add locale to cookie

--- lib/i18n_js.rb
+++ lib/i18n_js.rb
@@ -24,4 +24,21 @@ module I18nJs
     JAVASCRIPT
   end
 
+
+  module ControllerMethods
+
+    def self.included(controller)
+      controller.send(:after_filter, :set_locale_cookie)
+    end
+
+  private
+
+    def set_locale_cookie
+      cookies[:locale] = I18n.locale.to_s
+    end
+
+  end
+
 end
+
+ActionController::Base.send(:include, I18nJs::ControllerMethods)
