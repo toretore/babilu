be8a9dee0538a7b595f109cdeeda226be990b7d7
Tore Darell <toredarell@gmail.com>
2008-12-09 12:20:35 +0100

    Re-generate locale data on each request in development mode

--- lib/i18n_js.rb
+++ lib/i18n_js.rb
@@ -29,12 +29,18 @@ module I18nJs
 
     def self.included(controller)
       controller.send(:after_filter, :set_locale_cookie)
+      controller.send(:after_filter, :generate_locale_javascript) if Rails.env.development?
     end
 
   private
 
     def set_locale_cookie
       cookies[:locale] = I18n.locale.to_s
+    end
+
+    #In development mode, re-generate locale data on each request
+    def generate_locale_javascript
+      I18nJs.generate
     end
 
   end
