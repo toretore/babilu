27b01e43e7ea5797c186a2db04ff17afff7fd3c0
etagwerker <ernesto@etagwerker.com>
2011-02-24 18:19:46 -0300

    Added logic to filter all_translations using all references to I18n.t( or I18n.translate( in public. This improvement means that the generated file (locales.js) will be ligther than the whole locales.

--- lib/babilu.rb
+++ lib/babilu.rb
@@ -6,14 +6,58 @@ module Babilu
   def self.generate
     Lucy.generate("locales") do |g|
       g.namespace = "I18n"
-      g[:defaultLocale] = default_locale
-      g[:translations] = translations
+      g.write(:defaultLocale, default_locale)
+      g.write(:translations, translations)
       g << methods
     end
   end
 
+  # Searches for I18n.t calls and it extracts and array of strings
+  # to be used to generate the locales.js file
+  def self.grep_javascript_files
+    grep_output = ""
+    Dir.glob("public/**/**").select {|file| File.file?(file)}.each do |file|
+      File.open(file, 'r').each_with_index do |line, index|
+        grep_output += line if line.include?('I18n.t(') || line.include?('I18n.translate(')
+      end
+    end
+    result = []
+    result += grep_output.scan(/I18n\.t\('([\w+|\.+]+)'\)/) # captures I18n.t('a.key')
+    result += grep_output.scan(/I18n\.translate\('([\w+|\.+]+)'\)/) # captures I18n.t('a.key')    
+    result += grep_output.scan(/I18n\.t\(\"([\w+|\.+]+)\"\)/) # captures I18n.t("a.key")
+    result += grep_output.scan(/I18n\.translate\(\"([\w+|\.+]+)\"\)/) # captures I18n.translate("a.key")    
+    result.flatten
+  end
+
+  # Greps all the javascript files in public/
+  # and uses the I18n calls to generate locales.js
   def self.translations
-    I18n.all_translations
+    result = {}
+    needed_keys = grep_javascript_files
+    I18n.all_translations.each_key do |key|
+      result[key] = filter(I18n.all_translations[key], needed_keys)  
+    end
+    result
+  end
+
+  # the map will have all the translations 
+  # the keys_array will have strings like 'auctions.auction.current_price'
+  def self.filter(map, keys_array)
+    unless keys_array.empty?
+      filtered_map = map.select{ |key,value| keys_array.index { |string| string.index('.') ? string.start_with?("#{key}.") : string.start_with?("#{key}") }}
+      result = {}
+      filtered_map.each do |tuple|
+        key = tuple[0] # because hash#select returns an array
+        if map[key].class == Hash
+          result[key] = filter(map[key], keys_array.collect { |s| s.index('.') ? s[s.index('.') + 1, s.length] : nil}.compact)  
+        else
+          result[key] = map[key] if keys_array.include?(key.to_s)
+        end      
+      end
+      result      
+    else
+      nil
+    end
   end
 
   def self.default_locale
